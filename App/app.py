e8f21b7f76c198a1ff11e37eec377ab86d83f184
josepablito16 <josepablito16@gmail.com>
2020-11-12 06:18:06 +0000

    Se agrega la opcion para poder cargar la imagen que se quiere predecir, sin prediccion por el momento

--- App/app.py
+++ App/app.py
@@ -14,7 +14,7 @@ import dash
 import dash_bootstrap_components as dbc
 import dash_core_components as dcc
 import dash_html_components as html
-from dash.dependencies import Input, Output
+from dash.dependencies import Input, Output, State
 
 app = dash.Dash(external_stylesheets=[dbc.themes.BOOTSTRAP])
 
@@ -58,10 +58,62 @@ sidebar = html.Div(
     style=SIDEBAR_STYLE,
 )
 
+imagePicker = html.Div([
+    dcc.Upload(
+        id='upload-image',
+        children=html.Div([
+            'Drag and Drop or ',
+            html.A('Select Files')
+        ]),
+        style={
+            'width': '100%',
+            'height': '60px',
+            'lineHeight': '60px',
+            'borderWidth': '1px',
+            'borderStyle': 'dashed',
+            'borderRadius': '5px',
+            'textAlign': 'center',
+            'margin': '10px'
+        },
+        # Allow multiple files to be uploaded
+        multiple=True
+    ),
+    html.Div(id='output-image-upload'),
+])
+
 content = html.Div(id="page-content", style=CONTENT_STYLE)
 
 app.layout = html.Div([dcc.Location(id="url"), sidebar, content])
 
+
+def parse_contents(contents, filename, date):
+    # print(contents)
+
+    return html.Div([
+        # html.H5(filename),
+        # html.H6(datetime.datetime.fromtimestamp(date)),
+
+        # HTML images accept base64 encoded strings in the same format
+        # that is supplied by the upload
+        html.Img(src=contents),
+        html.Hr(),
+        html.Div('Raw Content'),
+        html.Pre(contents[0:200] + '...', style={
+            'whiteSpace': 'pre-wrap',
+            'wordBreak': 'break-all'
+        })
+    ])
+
+@app.callback(Output('output-image-upload', 'children'),
+              [Input('upload-image', 'contents')],
+              [State('upload-image', 'filename'),
+               State('upload-image', 'last_modified')])
+def update_output(list_of_contents, list_of_names, list_of_dates):
+    if list_of_contents is not None:
+        children = [
+            parse_contents(c, n, d) for c, n, d in
+            zip(list_of_contents, list_of_names, list_of_dates)]
+        return children
 
 # this callback uses the current pathname to set the active state of the
 # corresponding nav link to true, allowing users to tell see page they are on
@@ -83,7 +135,8 @@ def render_page_content(pathname):
     elif pathname == "/page-2":
         return html.P("Contenido del analisis de datos")
     elif pathname == "/page-3":
-        return html.P("Contenido de la prediccion")
+        return imagePicker
+        #return html.P("Contenido de la prediccion")
     # If the user tries to reach a different page, return a 404 message
     return dbc.Jumbotron(
         [
